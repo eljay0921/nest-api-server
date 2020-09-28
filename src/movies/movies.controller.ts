0baccd7973fb31bebd471b55e9e65a8dc3aae8fc
jin <lj7812@gmail.com>
2020-09-28 21:36:55 +0900

    # 2.1 init router

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies.';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieID: number) {
+    return `This will return one movie(${movieID})`;
+  }
+
+  @Post()
+  addOne() {
+    return `This will add a movie`;
+  }
+
+  @Delete('/:id')
+  removeOne(@Param('id') movieID: number) {
+    return `This will delete a movie(${movieID})`;
+  }
+
+  @Patch('/:id')
+  updateMovieInfo(@Param('id') movieID: number) {
+    return `This will update a movie(${movieID})'s some informations`;
+  }
+
+  @Put('/:id')
+  updateMovie(@Param('id') movieID: number) {
+    return `This will update a movie(${movieID})`;
+  }
+}
