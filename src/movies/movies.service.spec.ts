4735b6fc2fa9cd8a250c7e1043404d7870e5b3bc
jin <lj7812@gmail.com>
2020-10-04 16:35:59 +0900

    # 4.4 Test - updateOne()

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -4,6 +4,7 @@ import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  let movieId: number;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,6 +12,12 @@ describe('MoviesService', () => {
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+
+    movieId = service.addOne({
+      title: 'test title',
+      year: 2020,
+      genres: ['action', 'sports', 'drama'],
+    });
   });
 
   describe('Unit Test :: get all movies', () => {
@@ -22,18 +29,12 @@ describe('MoviesService', () => {
 
   describe('Unit Test :: get one movie', () => {
     it('should be return a movie information', () => {
-      const movieId = service.addOne({
-        title: 'test title',
-        year: 2020,
-        genres: ['action', 'sports', 'drama'],
-      });
-
       const movie = service.getOne(movieId);
       expect(movie).toBeDefined();
       expect(movie.title).toEqual('test title');
     });
 
-    it('should throw 404 error', () => {
+    it('should throw NotFoundException', () => {
       const nonExistId = 9999;
       try {
         service.getOne(nonExistId);
@@ -45,22 +46,14 @@ describe('MoviesService', () => {
   });
 
   describe('Unit Test :: remove one movie', () => {
-    let movieId = 0;
-
     it('should be remove', () => {
-      movieId = service.addOne({
-        title: 'test title',
-        year: 2020,
-        genres: ['action', 'sports', 'drama'],
-      });
-
       const beforeRemoveLen = service.getAll().length;
       service.removeOne(movieId);
       const afterRemoveLen = service.getAll().length;
       expect(afterRemoveLen).toBeLessThan(beforeRemoveLen);
     });
 
-    it('should throw 404 error', () => {
+    it('should throw NotFoundException', () => {
       try {
         service.removeOne(movieId);
       } catch (error) {
@@ -74,23 +67,44 @@ describe('MoviesService', () => {
     it('shoul add a movie', () => {
       const originMoviesLen = service.getAll().length;
 
-      const movieId = service.addOne({
-        title: 'test title',
-        year: 2020,
+      const secondMovieId = service.addOne({
+        title: 'test title 2',
+        year: 2021,
         genres: ['action', 'sports', 'drama'],
       });
 
-      expect(movieId).toEqual(1);
-      expect(movieId).toEqual(expect.any(Number));
+      expect(secondMovieId).toEqual(2);
+      expect(secondMovieId).toEqual(expect.any(Number));
 
       const addedMoviesLen = service.getAll().length;
 
       expect(addedMoviesLen).toBeGreaterThan(originMoviesLen);
 
-      const addedMovie = service.getOne(movieId);
-      expect(addedMovie.title).toEqual('test title');
-      expect(addedMovie.year).toEqual(2020);
+      const addedMovie = service.getOne(secondMovieId);
+      expect(addedMovie.title).toEqual('test title 2');
+      expect(addedMovie.year).toEqual(2021);
       expect(addedMovie.genres.length).toEqual(3);
+    });
+  });
+
+  describe('Unit Test :: update one movie', () => {
+    it('should update a movie', () => {
+      service.updateOne(movieId, { year: 2099 });
+
+      const updatedMovie = service.getOne(movieId);
+      expect(updatedMovie.id).toEqual(movieId);
+      expect(updatedMovie.title).toEqual('test title');
+      expect(updatedMovie.year).toEqual(2099);
+    });
+
+    it('should throw NotFoundException', () => {
+      const nonExistId = 9999;
+      try {
+        service.updateOne(nonExistId, { title: 'blah blah~' });
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(`Movie id ${nonExistId} is not found.`);
+      }
     });
   });
 });
